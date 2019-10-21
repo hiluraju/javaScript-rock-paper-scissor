4926b0cbdc09f3786e7be293a60303d855723b56
hiluraju <hiluraju@gmail.com>
2019-10-21 15:25:04 +0530

    js updates

--- js/main.js
+++ js/main.js
@@ -1,24 +1,98 @@
 const selections = document.querySelectorAll(".fa");
+const result     = { user : 0 , computer : 0};
+
+document.getElementById("output").style.display = "none";
 
 function playGame(e)
 {
-	let userSelection = e.srcElement.id;	
-	computerChoice();
+	let userSelection     = e.srcElement.id;	
+	let computerSelection = computerChoice();
+	let winner            = findWinner(userSelection,computerSelection);
+	ShowResult(winner,computerSelection);
+	// console.log(userSelection,computerSelection,winner);
 }
 
 function computerChoice()
 {
+	let randomVal = Math.random();
+	if(randomVal <= 0.33)
+	{
+		return "rock";
+	}
+	else if(randomVal <= 0.66)
+	{
+		return "paper";
+	}
+	else
+	{
+		return "scissor";	
+	}
+}
 
+function findWinner(user,comp)
+{
+	if(user == "rock")
+	{
+		if(comp == "rock")
+		{
+			return "draw";
+		}
+		else if(comp == "paper")
+		{
+			return "computer";
+		}
+		else
+		{
+			return "user";
+		}
+	}
+	else if(user == "paper")
+	{
+		if(comp == "rock")
+		{
+			return "user";
+		}
+		else if(comp == "paper")
+		{
+			return "draw";
+		}
+		else
+		{
+			return "computer";
+		}
+	}
+	else
+	{
+		if(comp == "rock")
+		{
+			return "computer";
+		}
+		else if(comp == "paper")
+		{
+			return "user";
+		}
+		else
+		{
+			return "draw";
+		}
+	}
 }
 
 
+function ShowResult(winner,computerSelection)
+{
+	document.getElementById("res").innerHTML 		    = `${winner} Wins!!!`;
+	document.getElementById("computerchoice").innerHTML = `Computer choose ${computerSelection}`;
+	document.getElementById("output").style.display     = "block";
+
+}
 
 //EventListner
 
 selections.forEach(function(selection)
-	{
-		selection.addEventListener("click", playGame)
-	});
+{
+	selection.addEventListener("click", playGame)
+});
 
 
 
